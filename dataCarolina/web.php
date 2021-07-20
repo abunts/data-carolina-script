060faad5d814023c3dcd1985d68ceed0a3f66a3f
abunts <abunta@me.com>
2021-07-20 17:47:25 -0400

    cURL session initialized

--- dataCarolina/web.php
+++ dataCarolina/web.php
@@ -1,12 +1,18 @@
-<script src="https://data.web.unc.edu/"></script>
+<!-- <script src="https://data.web.unc.edu/"></script> -->
+
 <?php
 //reading JSON file contents
-$data = file_get_contents("./newEvents.json");
+$eventData = file_get_contents("./newEvents.json");
 //converting JSON file contents to PHP associative array
-$data = json_decode($data, true);
- echo '<pre>';
- print_r($data);
+$jsonArray = json_decode($eventData, true);
+
+//service url
+ $url = 'https://data.web.unc.edu/';
+ //initializing curl session with service url as parameter
+ $curl = curl_init($url);
+ //setting the JSON content-type 
+ curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+ //sending the request 
+ curl_exec($curl);
  ?>
-
-
 
