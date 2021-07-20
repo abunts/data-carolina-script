f6118271f7b57b998435c211c53522fc49c36493
abunts <abunta@me.com>
2021-07-20 15:45:05 -0400

    php updates

--- dataCarolina/web.php
+++ dataCarolina/web.php
@@ -1,13 +1,12 @@
+<script src="https://data.web.unc.edu/"></script>
 <?php
 //reading JSON file contents
 $data = file_get_contents("./newEvents.json");
 //converting JSON file contents to PHP associative array
-$newEventsArray = json_decode(data, true);
-?>
+$data = json_decode($data, true);
+ echo '<pre>';
+ print_r($data);
+ ?>
 
-//echo data while defining a JS variable
-
-var nEvents = <?php echo data;?>
-console.log(nEvents);
 
 
