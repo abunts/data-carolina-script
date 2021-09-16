b951421a2e5f6dc5bb9cb94bf82574b23bf859db
Matt Watson <mattwat@gmail.com>
2021-09-16 14:13:48 -0400

    UI (#10)
    * install axios
    
    * decruft
    
    * store events in application state
    
    * render events count
    
    * render returned events
    
    * add simple error-checking
    
    * create events-list component
    
    * add events-list styling
    
    * add individual event styling
    
    * remove extraneous text
    
    * add search form items
    
    * wire up search form parts
    
    * relocate comment

---
+++ components/events-list.js
@@ -0,0 +1,18 @@
+import styles from '../styles/events.module.css'
+
+export const EventsList = ({ events }) => {
+  return (
+    <div className={ styles.eventsList }>
+      {
+        events.map((event, i) => {
+          return (
+            <div key={ i } className={ styles.event }>
+              <h3>{ event.name }</h3>
+              <p>{ event.description }</p>
+            </div>
+          )
+        })
+      }
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,62 @@
-import Head from 'next/head'
-import { useEffect } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import axios from 'axios'
+import { EventsList } from '../components/events-list'
+
 export default function Home() {
+  // variable to store the events in front-end's "state"
+  // `events` contains the event data
+  // `setEvents` is a function to assign the `events` variable
+  const [events, setEvents] = useState([])
+  const [error, setError] = useState(null)
+  const inputElement = useRef()
+
   useEffect(() => {
     const fetchEvents = async () => {
-      const response = await axios.get('/api/events');
-      if(!response.data){
-        return [];
+      try {
+        const response = await axios.get('/api/events');
+        if (!response.data) {
+          throw new Error('no data returned')
+          return [];
+        }
+        // here, we're using the `setEvents` function to, ...well, set events
+        // with the value of the `data` property in the response object.
+        setEvents(response.data);
+      } catch (error) {
+        setError(error)
+        console.error(error.message)
       }
-      console.log(response)
     }
-    fetchEvents()
-  }, [])
+    fetchEvents();
+  }, []);
+
+  // this function fires when the search button is clicked
+  const handleClickSearch = event => {
+    console.log(`\n\n\n\n the search button was clicked!\n\n\n\n`)
+    console.log(`the query string is "${ inputElement.current.value }"`)
+  }
+
+  if (error) {
+    return (
+      <div>an error occurred!</div>
+    )
+  }
+  
+  if (!events.length) {
+    return (
+      <div>no events!</div>
+    )
+  }
   
   return (
     <div>
-      <Head>
-        <title>Carolina Data Events</title>
-        <meta name="description" content="Carolina Data Science Events" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      
+      <div>
+        <input ref={ inputElement }/>
+        <button onClick={ handleClickSearch }>search</button>
+      </div>
+
+      <hr />
+      { events.length } events
+      <EventsList events={ events } />
     </div>
   )
 }
