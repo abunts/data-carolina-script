20e93695fb70b9627611fbcb787a396a4dc6aca3
abunts <abunta@me.com>
2021-12-16 17:19:12 -0500

    material UI features added

--- components/events-list.js
+++ components/events-list.js
@@ -3,12 +3,12 @@ import styles from '../styles/events.module.css'
 export const EventsList = ({ events }) => {
   return (
     <div className={ styles.eventsList }>
-      {
+      <p>Showing { events.length } results...</p>
+      { 
         events.map((event, i) => {
           return (
             <div key={ i } className={ styles.event }>
               <h3>{ event.name }</h3>
-              <p>{ event.description }</p>
               <div dangerouslySetInnerHTML={{ __html: event.description }} />
             </div>
           )
--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,19 @@ import { useEffect, useRef, useState } from 'react'
 import axios from 'axios'
 import { EventsList } from '../components/events-list'
 import fetch from "node-fetch";
+import { CircularProgress } from '@material-ui/core'
 
 export default function Home() {
   // variable to store the events in front-end's "state"
   // `events` contains the event data
   // `setEvents` is a function to assign the `events` variable
   const [events, setEvents] = useState([])
+  const [loading, setLoading] = useState(false)
   const [error, setError] = useState(null)
   const inputElement = useRef()
 
   useEffect(() => {
+    setLoading(true)
     const fetchEvents = async () => {
       try {
         const response = await axios.get('/api/events');
@@ -25,20 +28,28 @@ export default function Home() {
       } catch (error) {
         setError(error)
         console.error(error.message)
+      } finally {
+        setLoading(false)
       }
     }
     fetchEvents();
   }, []);
-  // this function fires when the search button is clicked
-  const api = '/api/events?pid={ inputElement.current.value }';
+
   const handleClickSearch = event => {
-    console.log(`\n\n\n\n the search button was clicked!\n\n\n\n`)
-    console.log(`the query string is "${ inputElement.current.value }"`)
-    // the value inside the textbox is inputElement.current.value
-    // send request to api at this URI:
-    //   `/api/events?pid=${ inputElement.current.value }`
-    // you'll get JSON back with filtered events.
-   fetch(api);
+    event.preventDefault();
+    setLoading(true);
+    fetch(`/api/events?pid=${ inputElement.current.value }`)
+      .then(response => response.json())
+      .then(events => {
+        setEvents(events)
+      })
+      .catch(error => {
+        console.log(error)
+        setEvents([])
+      })
+      .finally(() => {
+        setLoading(false)
+      })
   }
 
   if (error) {
@@ -47,20 +58,22 @@ export default function Home() {
     )
   }
   
-  if (!events.length) {
+  if (!events.length && !loading) {
     return (
       <div>no events!</div>
     )
   }
+
   return (
     <div>
-      <div>
+      <form onSubmit={ handleClickSearch }>
         <input ref={ inputElement }/>
-        <button onClick={ handleClickSearch }>search</button>
-      </div>
+        <button>search</button>
+      </form>
       <hr />
-      { events.length } events
-      <EventsList events={ events } />
+      { loading 
+      ? <CircularProgress /> 
+      : <EventsList events={ events } /> }
     </div>
   )
 }
